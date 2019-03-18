add51003c546636fd786da8bee760689a41e702d
mpurang <meghnap111@gmail.com>
2019-03-18 16:42:10 +1300

    load files and open data

--- chatbot.py
+++ chatbot.py
@@ -4,3 +4,9 @@ import os
 
 bot = ChatBot("Bot") # define bot
 bot.set_trainer(ListTrainer)      # set trainer to train the bot
+
+# load files available in the local system. Have already downloaded chatterbot-corpus
+for files in os.listdir("C:/Users/Owner\Downloads\chatterbot-corpus-master\chatterbot-corpus-master\chatterbot_corpus\data\english/"):
+    # open data using the same path
+    data = open("C:/Users/Owner\Downloads\chatterbot-corpus-master\chatterbot-corpus-master\chatterbot_corpus\data\english/" + files, "r").readlines()
+
