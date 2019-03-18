0f087375173c689ec3de7644da84182d9ca87730
mpurang <meghnap111@gmail.com>
2019-03-18 16:40:55 +1300

    define bot and set trainer

--- chatbot.py
+++ chatbot.py
@@ -2,3 +2,5 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 import os
 
+bot = ChatBot("Bot") # define bot
+bot.set_trainer(ListTrainer)      # set trainer to train the bot
